746e1b29142e1788f0212bcb16c4a4cce3923de6
Sertac Ozercan <sozercan@gmail.com>
2024-05-25 20:09:42 +0000

    update
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- backend/python/exllama/run.sh
+++ backend/python/exllama/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LIMIT_TARGETS="cublas"
-# BACKEND_FILE="${MY_DIR}/source/backend.py"
+BACKEND_FILE=${BACKEND_FILE:-"${MY_DIR}/source/backend.py"}
 
 source $(dirname $0)/../common/libbackend.sh
 
